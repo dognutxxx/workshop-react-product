8074714962664aac4fd1a7a8882113c4fe8ae84c
arnonaoc <arnon@asia1click.com>
2023-03-05 10:48:45 +0700

    first commit

---
+++ src/NoPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class NoPage extends Component {
+  render() {
+    return (
+      <div>
+        <div align="center">
+          <h1>Error Contentd</h1>
+        </div>
+      </div>
+    );
+  }
+}
